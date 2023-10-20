0229b73883439a5a21fc0ff1a1d4404722884b3a
EliaBuratti <elia.buratti.dev@gmail.com>
2023-10-20 15:02:46 +0200

    initial commit
    create file php, not connected

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+/* Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+L'e-commerce vende prodotti per animali.
+I prodotti sono categorizzati, le categorie sono Cani o Gatti.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
+Bonus (non opzionale):
+organizzate il progetto come visto stamattina a lezione usando varie sottocartelle per inserire classi, layout e dati. */
+
+
+class Animal
+{
+}
+
+class Product
+{
+}
+
+class Food
+{
+}
+
+class Toys
+{
+}
+
+class Leash
+{
+}
+
+class Kennel_Mats
+{
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Store</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
